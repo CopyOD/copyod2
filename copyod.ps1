41b7beb0120a66d3de086352c6b2e44d2838e063
YAM <89498399+nxzu@users.noreply.github.com>
2021-10-25 19:02:15 +0800

    Update copyod.ps1

--- copyod.ps1
+++ copyod.ps1
@@ -20,6 +20,25 @@ while($true){
 		Connect-PnPOnline -Url $OneDriveSite -Credentials $MySecureCreds
 		If (-Not $?) { Write-Host "Error: PnPOnline Authentication." -ForegroundColor red; Exit 1; }
 	
+		Write-Host "Upload File to $OneDriveSite"
+		$FileName = -join ([char[]](65..90) | Get-Random -Count 4)
+		$FileSize = Get-Random -Maximum 550 -Minimum 500
+		dd if=/dev/zero of=$FileName bs=1M count=0 seek=$FileSize 2>&1>$null
+		Add-PnPFile -Path $FileName -Folder "Documents" 2>&1>$null
+		Remove-Item $FileName -Force
+	
+		Resolve-PnPFolder -SiteRelativePath "Documents/FirstFolder" 2>&1>$null
+		for($i=1;$i -le 100;$i++){
+			Write-Progress -Activity "Copy files..." -Status "$i% Complete:" -PercentComplete $i
+			$NewFileName = -join ([char[]](65..90) | Get-Random -Count 8)
+			Copy-PnPFile -SourceUrl ${RootDirectory}${FileName} -TargetUrl ${FirstFolder}${NewFileName} -OverwriteIfAlreadyExists -Force -ErrorAction Stop
+		}
+	
+		for($i=1;$i -le 100;$i++){
+			Write-Progress -Activity "Copy folders..." -Status "$i% Complete:" -PercentComplete $i
+			$NewFolderName = -join ([char[]](65..90) | Get-Random -Count 8)
+			Copy-PnPFile -SourceUrl $FirstFolder -TargetUrl ${RootDirectory}${NewFolderName} -OverwriteIfAlreadyExists -Force -ErrorAction SilentlyContinue
+		}
 	
 		Disconnect-PnPOnline
 	}
