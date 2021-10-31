81487d79e1c1826de660963717c40b826f94c8d9
YAM <89498399+nxzu@users.noreply.github.com>
2021-10-31 23:02:14 +0800

    Update copyod.ps1

--- copyod.ps1
+++ copyod.ps1
@@ -1,60 +1,115 @@
-Write-Host "这是一个通过复制文件快速塞满OneDrive 5TB的实例"
-Write-Host "登陆全局默认域管理员账号：XX@XXX.onmicrosoft.com"
-Write-Host "-------------------------------------------------------"
-Do { $AdminUser = (Read-Host "Microsoft Office365 AdminUser") } While ([String]::IsNullOrEmpty($($AdminUser).Trim())) 
-Do { $AdminPwd = (Read-Host "Microsoft Office365 AdminPwd") } While ([String]::IsNullOrEmpty($($AdminPwd).Trim()))
+#Function to Upload Large File to SharePoint Online Library
+Function Upload-LargeFile($FilePath, $LibraryName, $FileChunkSize=50) {
+    Try {
+        #Get File Name
+        $FileName = [System.IO.Path]::GetFileName($FilePath)
+        $UploadId = [GUID]::NewGuid()
+		
+        #Get the folder to upload
+        $Library = $Ctx.Web.Lists.GetByTitle($LibraryName)
+        $Ctx.Load($Library)
+        $Ctx.Load($Library.RootFolder)
+        $Ctx.ExecuteQuery()
+ 
+        $BlockSize = $FileChunkSize * 1024 * 1024  
+        $FileSize = (Get-Item $FilePath).length
+        If($FileSize -le $BlockSize) {
+            #Regular upload
+            $FileStream = New-Object IO.FileStream($FilePath,[System.IO.FileMode]::Open)
+            $FileCreationInfo = New-Object Microsoft.SharePoint.Client.FileCreationInformation
+            $FileCreationInfo.Overwrite = $true
+            $FileCreationInfo.ContentStream = $FileStream
+            $FileCreationInfo.URL = $FileName
+            $Upload = $Docs.RootFolder.Files.Add($FileCreationInfo)
+            $ctx.Load($Upload)
+            $ctx.ExecuteQuery()
+        }
+        Else {
+            #Large File Upload in Chunks
+            $ServerRelativeUrlOfRootFolder = $Library.RootFolder.ServerRelativeUrl
+            [Microsoft.SharePoint.Client.File]$Upload
+            $BytesUploaded = $null 
+            $Filestream = $null
+            $Filestream = [System.IO.File]::Open($FilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite)
+            $BinaryReader = New-Object System.IO.BinaryReader($Filestream)
+            $Buffer = New-Object System.Byte[]($BlockSize)
+            $LastBuffer = $null
+            $Fileoffset = 0
+            $TotalBytesRead = 0
+            $BytesRead
+            $First = $True
+            $Last = $False
+ 
+            #Read data from the file in blocks
+			$i = 0
+            While(($BytesRead = $BinaryReader.Read($Buffer, 0, $Buffer.Length)) -gt 0) { 
+				$i = $i+1
+				Write-Host $i
+				#Write-Progress -Activity "Searching Events" -Status "Progress:" -PercentComplete ($i/$Events.count*100)
+                #Write-Progress -Activity "Copy files..." -Status "$i% Complete:" -PercentComplete $i
+				$TotalBytesRead = $TotalBytesRead + $BytesRead 
+                If ($TotalBytesRead -eq $FileSize) {  
+                    $Last = $True
+                    $LastBuffer = New-Object System.Byte[]($BytesRead)
+                    [Array]::Copy($Buffer, 0, $LastBuffer, 0, $BytesRead)  
+                }
+                If($First) {  
+                    #Create the File in Target
+                    $ContentStream = New-Object System.IO.MemoryStream
+                    $FileCreationInfo = New-Object Microsoft.SharePoint.Client.FileCreationInformation
+                    $FileCreationInfo.ContentStream = $ContentStream
+                    $FileCreationInfo.Url = $FileName
+                    $FileCreationInfo.Overwrite = $true
+                    $Upload = $Library.RootFolder.Files.Add($FileCreationInfo)
+                    $Ctx.Load($Upload)
+ 
+                    #Start FIle upload by uploading the first slice
+                    $s = new-object System.IO.MemoryStream(, $Buffer)  
+                    $BytesUploaded = $Upload.StartUpload($UploadId, $s)
+                    $Ctx.ExecuteQuery()  
+                    $fileoffset = $BytesUploaded.Value  
+                    $First = $False 
+                }  
+                Else {  
+                    #Get the File Reference
+                    $Upload = $ctx.Web.GetFileByServerRelativeUrl($Library.RootFolder.ServerRelativeUrl + [System.IO.Path]::AltDirectorySeparatorChar + $FileName);
+                    If($Last) {
+                        $s = [System.IO.MemoryStream]::new($LastBuffer)
+                        $Upload = $Upload.FinishUpload($UploadId, $fileoffset, $s)
+                        $Ctx.ExecuteQuery()
+                        Write-Host "File Upload completed!" -f Green                        
+                    }
+                    Else {
+                        #Update fileoffset for the next slice
+                        $s = [System.IO.MemoryStream]::new($buffer)
+                        $BytesUploaded = $Upload.ContinueUpload($UploadId, $fileoffset, $s)
+                        $Ctx.ExecuteQuery()
+                        $fileoffset = $BytesUploaded.Value
+                    }
+                }
+            }
+        }
+    }
+    Catch {
+        Write-Host $_.Exception.Message -ForegroundColor Red
+    }
+    Finally {
+        If($Filestream -ne $null)
+        {
+            $Filestream.Dispose()
+        }
+    }
+}
+ 
+#Connect to SharePoint Online site
+$AdminUser = "admin@sldoz.onmicrosoft.com"
+$AdminPwd = "As8520342"
 $SecureString = ConvertTo-SecureString -AsPlainText "${AdminPwd}" -Force
 $MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ${AdminUser},${SecureString}
-
-try {
-	az login -u $AdminUser -p $AdminPwd --allow-no-subscriptions --only-show-errors | Out-Null
-	$PnPPowerShellAppId = "31359c7f-bd7e-475c-86db-fdb8c937548e"
-	$existUAPI = az ad sp show --id $PnPPowerShellAppId 2>&1>$null
-	If ([String]::IsNullOrEmpty($existUAPI)) {
-		az ad sp create --id $PnPPowerShellAppId 2>&1>$null
-		az ad app permission grant --id $PnPPowerShellAppId --api 00000003-0000-0000-c000-000000000000 --scope User.Read.All,Group.ReadWrite.All 2>&1>$null
-		az ad app permission grant --id $PnPPowerShellAppId --api 00000003-0000-0ff1-ce00-000000000000 --scope User.Read.All,Sites.FullControl.All 2>&1>$null
-	}
-	
-	$UserORG = ($AdminUser -Split {$_ -eq "@" -or $_ -eq "."})[1]
-	$OneDriveSite = "https://{0}-my.sharepoint.com/personal/xs_{0}_onmicrosoft_com" -f $UserORG
-	$RootDirectory = "Documents/copyod/"
-	$FirstFolder = $RootDirectory + "FirstFolder/"
-	Connect-PnPOnline -Url $OneDriveSite -Credentials $MySecureCreds
-	Set-PnPTenantSite -Identity $OneDriveSite -Owners $AdminUser
-	
-	Write-Host "Upload File to $OneDriveSite"
-	$FileName = -join ([char[]](65..90) | Get-Random -Count 4)
-	$FileSize = Get-Random -Maximum 530 -Minimum 500
-	dd if=/dev/zero of=$FileName bs=1M count=$FileSize 2>&1>$null
-	Add-PnPFile -Path $FileName -Folder $RootDirectory
-	Remove-Item $FileName -Force
-	Resolve-PnPFolder -SiteRelativePath $FirstFolder
-	
-	for($i=1;$i -le 100;$i++){
-		Write-Progress -Activity "Copy files..." -Status "$i% Complete:" -PercentComplete $i
-		$NewFileName = -join ([char[]](65..90) | Get-Random -Count 8)
-		Copy-PnPFile -SourceUrl ${RootDirectory}${FileName} -TargetUrl ${FirstFolder}${NewFileName} -OverwriteIfAlreadyExists -Force -ErrorAction Stop
-	}
-
-	for($i=1;$i -le 100;$i++){
-		Write-Progress -Activity "Copy folders..." -Status "$i% Complete:" -PercentComplete $i
-		$NewFolderName = -join ([char[]](65..90) | Get-Random -Count 8)
-		Copy-PnPFile -SourceUrl $FirstFolder -TargetUrl ${RootDirectory}${NewFolderName} -OverwriteIfAlreadyExists -Force -ErrorAction SilentlyContinue
-	}
-	
-	$GetSPO = Get-PnPTenantSite -Url $OneDriveSite
-	$UsageAmount = [math]::Round($GetSPO.StorageUsageCurrent / $GetSPO.StorageQuota * 100,2)
-	Write-Host "User: $($GetSPO.Owner), StorageQuota: $($($GetSPO.StorageQuota) / 1024 / 1024)TB, UsageAmount：$UsageAmount%" -ForegroundColor Green
-	Remove-PnPSiteCollectionAdmin -Owners ($AdminUser -Split {$_ -eq "@" -or $_ -eq "."})[0]
-	
-	az account clear
-	Disconnect-PnPOnline
-}
-catch {
-	Write-Host "Error: $_" -ForegroundColor Red
-}
-Finally {
-	Pause
-	Exit 1
-}
+$OneDriveSite = "https://sldoz-my.sharepoint.com/personal/xs_sldoz_onmicrosoft_com"
+Connect-PnPOnline -Url $OneDriveSite -Credentials $MySecureCreds
+#Connect-PnPOnline "https://crescent.sharepoint.com/sites/marketing" -UseWebLogin
+$Ctx = Get-PnPContext
+ 
+#Call the function to Upload File
+Upload-LargeFile -FilePath "E:\Win7PE" -LibraryName "Documents"
